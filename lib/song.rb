1775329f13af3fd7b48c1844d700841d0789f71b
Adam Urbina <adamurbina@gmail.com>
2018-06-06 17:53:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,13 +12,20 @@ class Song
 
   def self.create
       song = Song.new
-      @@all << song
+      self.class.all << song
       song
   end
 
   def self.new_by_name(name)
       song = Song.new
       song.name = name
+      song
+  end
+
+  def self.create_by_name(name)
+      song = Song.new
+      song.name = name
+      self.class.all << song
       song
   end
 
